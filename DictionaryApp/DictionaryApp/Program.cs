4d98d79174b8f8a1fbacce4319225af5a23e6f41
fk55 <76855467+fk55@users.noreply.github.com>
2022-04-08 17:38:08 +0300

    DictionaryApp With Windows Forms

---
+++ DictionaryApp/DictionaryApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DictionaryApp
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+
+           
+
+            Application.Run(new Form1(GetDictionary()));
+        }
+
+        public static SortedDictionary<string, string> GetDictionary()
+        {
+            SortedDictionary<string, string> dictionary = new SortedDictionary<string, string>();
+            SqlConnection conn = db.conn();
+            SqlCommand cmd = new SqlCommand("Select * from Dictionary", conn);
+            conn.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                dictionary.Add(dr[0].ToString(), dr[1].ToString());
+            }
+            dr.Close();
+            conn.Close();
+            return dictionary;
+        }
+    }
+}
---
+++ DictionaryApp/DictionaryApp/db.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DictionaryApp
+{
+    public static class db
+    {
+        public static SqlConnection conn()
+        {
+            SqlConnection conn = new SqlConnection(@"Server=DESKTOP-MO5HLC4\SQLEXPRESS; Database=fk; Trusted_Connection=True;" + "Integrated Security=true;");
+            return conn;
+        }
+        
+    }
+}
